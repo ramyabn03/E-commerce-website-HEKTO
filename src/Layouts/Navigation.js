b6a393c7f940980b9d691b017bb1fa3695fe7b0e
Ramya B N <84955636+ramyabn03@users.noreply.github.com>
2023-03-15 19:44:01 +0530

    Update Navigation.js
    Added Link to nav Bar

--- src/Layouts/Navigation.js
+++ src/Layouts/Navigation.js
@@ -9,9 +9,20 @@ import { BsCart2 } from "react-icons/bs";
 import { TbSearch } from "react-icons/tb";
 import { RiCloseLine } from "react-icons/ri";
 import { FiMenu } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
+
+  function handelnavchange(id) {
+    let ids = ["home", 'blog', 'myblog', 'account','about', 'cart']
+    ids.forEach(each => {
+      if(each !== id){
+      document.getElementById(each).classList.remove('active');}
+    })
+    document.getElementById(id).classList.add('active')
+  }
+
 
   return (
     <Fragment>
@@ -74,14 +85,16 @@ export const Navigation = () => {
                 : "flex-center nav-bar-options"
             }
           >
-            <a href="/#" className="active">
+            <Link to="/" className="active added-link" id="home" onClick={() => handelnavchange("home")}>
               Home <i className="fa fa-angle-down"></i>
-            </a>
-            <a href="/#">Pages</a>
-            <a href="/#">Products</a>
-            <a href="/#">Blog</a>
-            <a href="/#">Shop</a>
-            <a href="/#">Contact</a>
+            </Link>
+            {/* <a href="/#">Pages</a> */}
+            {/* <a href="/#">Products</a> */}
+            <Link className="added-link" to="/blog-page" id="blog" onClick={() => handelnavchange("blog")}>Blog</Link>
+            <Link className="added-link" to="/single-blog-page" id="myblog" onClick={() => handelnavchange("myblog")}>MyBlog</Link>
+            <Link className="added-link" to="/my-account"id="account" onClick={() => handelnavchange("account")}>Login</Link>
+            <Link className="added-link" to="/about-us"id="about" onClick={() => handelnavchange("about")}>About</Link>
+            <Link className="added-link" to="/shopping-cart"id="cart" onClick={() => handelnavchange("cart")}>Cart</Link>
           </div>
           <div className="mobile-menu">
             <button
